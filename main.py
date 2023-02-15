900f2d25bfb148f43e183f632630006bcb07ef08
thecodingpie <63698375+the-coding-pie@users.noreply.github.com>
2023-02-15 21:55:52 +0530

    empty token

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from time import sleep
 name = "Arvind"
 
 # open ai configs
-openai.api_key = "" # your openai api key here
+openai.api_key = "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" # your openai api key here
 model_engine = "text-davinci-003"
 
 # instance of sr Recognizer
@@ -58,7 +58,7 @@ def reply(text):
         speak("Speak something")
         return
 
-    if "bye" in text:
+    if "shutdown" in text:
         speak(f"Good bye {name}")
         exit()
     
